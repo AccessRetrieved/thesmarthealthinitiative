7340c91c9dd26fe1c952b805d028018e83da1550
Jerry Hu <work.jerrywu@gmail.com>
2024-09-14 20:32:06 -0700

    working version (linear regression)

--- linear_regression/plot.py
+++ linear_regression/plot.py
@@ -4,7 +4,10 @@ import sys
 import pandas as pd
 import joblib
 sys.path.append('../thesmarthealthinitiative')
+from data import *
 model = joblib.load('life_expectancy_model.pkl')
+
+data = load_file('neighborhood_health_and_wellness.csv')
 
 def scatter_plot(data, x, y):
     plt.figure(figsize=(10, 6))
@@ -12,7 +15,17 @@ def scatter_plot(data, x, y):
     plt.title('Average Income vs Life Expectancy')
     plt.xlabel('Average Income')
     plt.ylabel('Life Expectancy')
-    plt.grid(1)
+    plt.grid(True)
     plt.show()
 
+def box_plot(data, x, y):
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.boxplot(data['Average Income'])
+    plt.title('Box Plot of Average Income')
+    plt.ylabel('Average Income')
+    plt.boxplot(data['Life Expectancy'])
+    plt.tight_layout()
+    plt.show()
 
+scatter_plot(data, data['Average Income'], data['Life Expectancy'])
